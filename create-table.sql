ea50a6847e5778e940e82316b71ac7f4621c37b6
kahmadsyt <achmadkamil17@gmail.com>
2021-08-20 01:08:34 +0700

    PENAMBAHAN-QUERY

--- create-table.sql
+++ create-table.sql
@@ -9,3 +9,24 @@ CREATE TABLE barang
     waktu_dibuat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 ENGINE = InnoDB;
+
+SHOW TABLES;
+DESCRIBE barang;
+
+ALTER TABLE barang
+ADD column deskripsi TEXT;
+
+ALTER TABLE barang
+ADD column contoh_salah TEXT;
+
+ALTER TABLE barang
+DROP column contoh_salah;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) AFTER deskripsi;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) FIRST;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) NOT NULL;
