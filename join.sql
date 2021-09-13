ed9569c7171dd730fcd960656374f457687c812b
kahmadsyt <achmadkamil17@gmail.com>
2021-09-13 21:31:50 +0700

    join-clause

---
+++ join.sql
@@ -0,0 +1,58 @@
+SHOW DATABASES;
+USE belajar_mysql;
+
+SHOW TABLES;
+DESC wishlist;
+
+SELECT *
+FROM wishlist;
+
+SELECT *
+FROM wishlist
+JOIN products ON (wishlist.id_product = products.id);
+
+SELECT wishlist.id, products.id, products.name, wishlist.description
+FROM wishlist
+JOIN products ON (wishlist.id_product = products.id);
+
+SELECT wl.id, pr.id, pr.name, wl.description
+FROM wishlist AS wl
+JOIN products AS pr ON (wl.id_product = pr.id);
+
+SELECT	wl.id AS wishlist_id,
+		pr.id AS products_id,
+        pr.name AS products_name,
+        wl.description AS wishlist_description
+FROM wishlist AS wl
+JOIN products AS pr ON (wl.id_product = pr.id);
+
+DESC customers;
+
+ALTER TABLE wishlist
+ADD COLUMN id_customer INT;
+
+DESC wishlist;
+
+SELECT *
+FROM customers;
+
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer
+FOREIGN KEY(id_customer) REFERENCES customers(id);
+
+UPDATE wishlist
+SET id_customer = 1
+WHERE id = 9;
+
+SELECT *
+FROM wishlist;
+
+SELECT	customers.email AS cust_email,
+		customers.firstname AS cust_firstname,
+        customers.lastname AS cust_lastname,
+        products.id AS prod_id,
+        products.name AS prod_name,
+        wishlist.description AS wishlist_description
+FROM wishlist
+JOIN products ON (products.id = wishlist.id_product)
+JOIN customers ON (customers.id = wishlist.id_customer);
